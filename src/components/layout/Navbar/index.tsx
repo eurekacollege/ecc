818cb026ba458739cd36a4831bc3961fdd0488fd
emilsangil <sangilemiljan@gmail.com>
2026-02-24 12:27:26 -0800

    building navbar

---
+++ src/components/layout/Navbar/index.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./Navbar.module.css";
+
+export default function Navbar () {
+    const [ show, setShow ] = useState(false);
+    function openSideBar() {
+        setShow((prev) => !prev);
+    }
+    return (
+        <>
+            <button onClick={openSideBar}>
+                <svg id={styles.openSidebarButton} className={show ? styles.show : ""} xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#c9c9c9"><path d="M165.13-254.62q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.86q7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.87q-7.22 7.12-17.9 7.12H165.13Zm0-200.25q-10.68 0-17.9-7.27-7.23-7.26-7.23-17.99 0-10.74 7.23-17.87 7.22-7.13 17.9-7.13h629.74q10.68 0 17.9 7.27 7.23 7.26 7.23 17.99 0 10.74-7.23 17.87-7.22 7.13-17.9 7.13H165.13Zm0-200.26q-10.68 0-17.9-7.26-7.23-7.26-7.23-18t7.23-17.87q7.22-7.12 17.9-7.12h629.74q10.68 0 17.9 7.26 7.23 7.26 7.23 18t-7.23 17.86q-7.22 7.13-17.9 7.13H165.13Z"/></svg>
+            </button>
+            <nav className={styles.navbar}>
+                <ul>
+                    <li><Image className={styles.logo} src="/globe.svg" alt="logo" width={100} height={100}/></li>
+                    <li><button><svg id={styles.closeSidebarButton} xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#c9c9c9"><path d="m480-444.62-209.69 209.7q-7.23 7.23-17.5 7.42-10.27.19-17.89-7.42-7.61-7.62-7.61-17.7 0-10.07 7.61-17.69L444.62-480l-209.7-209.69q-7.23-7.23-7.42-17.5-.19-10.27 7.42-17.89 7.62-7.61 17.7-7.61 10.07 0 17.69 7.61L480-515.38l209.69-209.7q7.23-7.23 17.5-7.42 10.27-.19 17.89 7.42 7.61 7.62 7.61 17.7 0 10.07-7.61 17.69L515.38-480l209.7 209.69q7.23 7.23 7.42 17.5.19 10.27-7.42 17.89-7.62 7.61-17.7 7.61-10.07 0-17.69-7.61L480-444.62Z"/></svg></button></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/program">Program</a></li>
+                    <li><a href="/policies">Policies</a></li>
+                    <li><a href="/news&events">News & Events</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
