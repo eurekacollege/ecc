8c305587cc817e7724d68521fdc2934806e77306
eurekacollege <eurekacollegecanada@gmail.com>
2026-03-06 23:42:41 -0800

    update images of event and program cards

--- src/data/events.ts
+++ src/data/events.ts
@@ -7,12 +7,12 @@ export const Events = [
     },
     {
        title: "Supporting Community Last Christmas",
-       image: "/images/event_donation.jpg",
+       image: "/images/event_donation.png",
        description:"Coming together to give back and support those in our community during the holiday season." 
     },
     {
        title: "New Diploma Program",
-       image: "/images/event_new_program.jpg",
+       image: "/images/event_new_program.png",
        description:"Potential Workplace: Hospitals, Clinics, Physician’s office, Other healthcare settings​" 
     },
 ]
--- src/data/programs.ts
+++ src/data/programs.ts
@@ -7,28 +7,28 @@ export const Programs = [
         link:""
     },
     {
-        image: "/images/Programs_MOA.jpeg",
+        image: "/images/Programs_MOA.png",
         alt: "community support worker",
         title: "Medical Office Administrator",
         description: "Community Support Worker are in high demand; in only six months you could be earning up to $25 per hour! Monthly intakes, online courses, and a variety of schedule options to choose from.",
         link:""
     },
     {
-        image: "/images/Programs_CELBAN.jpeg",
+        image: "/images/Programs_CELBAN.png",
         alt: "community support worker",
         title: "CELBAN",
         description: "Community Support Worker are in high demand; in only six months you could be earning up to $25 per hour! Monthly intakes, online courses, and a variety of schedule options to choose from.",
         link:""
     },
     {
-        image: "/images/Programs_ECEA.jpeg",
+        image: "/images/Programs_ECEA.png",
         alt: "community support worker",
         title: "Early Childhood Education Assistant",
         description: "Community Support Worker are in high demand; in only six months you could be earning up to $25 per hour! Monthly intakes, online courses, and a variety of schedule options to choose from.",
         link:""
     },
     {
-        image: "/images/Programs_CaregiverSupportWorker.jpeg",
+        image: "/images/Programs_CaregiverSupportWorker.png",
         alt: "community support worker",
         title: "Caregiver Support Worker",
         description: "Community Support Worker are in high demand; in only six months you could be earning up to $25 per hour! Monthly intakes, online courses, and a variety of schedule options to choose from.",
